491d5198f45dfb879c08042cedb85ca6de99b3a7
MusLG <luke.muschialli.20@ucl.ac.uk>
2022-11-26 15:53:32 +0000

    New

--- Update_Existing_Form_Admin_GUI.py
+++ Update_Existing_Form_Admin_GUI.py
@@ -13,13 +13,16 @@ def UpdateEmergencyScreen():
     global Update_Emergency_Home_Screen
     global Update_Emergency_Screen
     global emergency_database_list
-    global view_table_check_no
+    global View_Table_Yes
     Update_Emergency_Screen = Toplevel(Update_Emergency_Home_Screen)
     Update_Emergency_Screen.title("Update an existing emergency")
     Update_Emergency_Screen.geometry("500x600")
 
     Update_Emergency_Screen_Label = Label(Update_Emergency_Screen, text="You are going to update an existing emergency plan. Please follow the below instructions")
     Update_Emergency_Screen_Label.pack()
+
+    View_Table_Label = Label(Update_Emergency_Screen, text="Would you like to view a summary of all of the projects currently in the database?")
+    View_Table_Label.pack()
 
     View_Table_Yes = IntVar()
     View_Table_No = IntVar()
@@ -33,8 +36,8 @@ def UpdateEmergencyScreen():
 def ViewTable():
     global Update_Emergency_Screen
     global emergency_database_list
-    global view_table_check_no
-    global index_known_no
+    global View_Table_Yes
+    global Index_Known_No
 
     emergency_database_frame = Frame(Update_Emergency_Screen)
     emergency_database_frame.pack()
@@ -69,16 +72,16 @@ def ViewTable():
 
     emergency_database_table.pack()
 
-    if view_table_check_no.get() == 1:
+    if View_Table_Yes.get() == 1:
         IndexKnown()
-    if index_known_no.get() == 1:
+    if Index_Known_No.get() == 1:
         SelectIndex()
     else:
         IndexKnown()
 
 def IndexKnown():
     global Update_Emergency_Screen
-    global index_known_no
+    global Index_Known_No
 
     Index_Known_Yes = IntVar()
     Index_Known_No = IntVar()
@@ -108,18 +111,63 @@ def SelectIndex():
     select_index_select = OptionMenu(Update_Emergency_Screen, select_index, *emergency_database_list_index)
     select_index_select.pack()
 
+    select_index_select_button = Button(Update_Emergency_Screen, text="Submit", command=printupdatingCamp)
+    select_index_select_button.pack()
 
-    printupdatingCamp()
 
 def printupdatingCamp():
     global Update_Emergency_Screen
     global emergency_database_list
+    global select_index
     global index_updating_camp
+    global emergency_database_list
 
-    #Selecting index currently not working
 
     index_updating_camp = select_index.get()
-    print(index_updating_camp)
+    index_updating_camp = int(index_updating_camp)
+
+    updating_camp_list = []
+    for i in range(0, 7):
+        updating_camp_list.append((emergency_database_list[index_updating_camp])[i])
+        i += 1
+
+    print(updating_camp_list)
+
+    index_select_label = Label(Update_Emergency_Screen, text="You are updating camp %d"%(index_updating_camp))
+    index_select_label.pack()
+
+    update_emergency_frame = Frame(Update_Emergency_Screen)
+    update_emergency_frame.pack()
+
+    update_emergency_table = ttk.Treeview(update_emergency_frame)
+
+    update_emergency_table['columns'] = ("Camp ID", "Camp Name", "Emergency Type", "Emergency Description", "Area Affected", "Start Date", "Close Date", "Status")
+
+    update_emergency_table.column("#0", width=0, stretch=NO)
+    update_emergency_table.column("Camp ID", anchor=CENTER, width=100)
+    update_emergency_table.column("Camp Name", anchor=CENTER, width=100)
+    update_emergency_table.column("Emergency Type", anchor=CENTER, width=100)
+    update_emergency_table.column("Emergency Description", anchor=CENTER, width=100)
+    update_emergency_table.column("Area Affected", anchor=CENTER, width=100)
+    update_emergency_table.column("Start Date", anchor=CENTER, width=100)
+    update_emergency_table.column("Close Date", anchor=CENTER, width=100)
+    update_emergency_table.column("Status", anchor=CENTER, width=100)
+
+    update_emergency_table.heading("Camp ID", text="Camp ID", anchor=CENTER)
+    update_emergency_table.heading("Camp Name", text="Camp Name", anchor=CENTER)
+    update_emergency_table.heading("Emergency Type", text="Emergency Type", anchor=CENTER)
+    update_emergency_table.heading("Emergency Description", text="Emergency Description", anchor=CENTER)
+    update_emergency_table.heading("Area Affected", text="Area Affected", anchor=CENTER)
+    update_emergency_table.heading("Start Date", text="Start Date", anchor=CENTER)
+    update_emergency_table.heading("Close Date", text="Close Date", anchor=CENTER)
+    update_emergency_table.heading("Status", text="Status", anchor=CENTER)
+
+
+    update_emergency_table.insert(parent='', index=i, iid=i, values=(updating_camp_list[0], updating_camp_list[1], updating_camp_list[2], updating_camp_list[3], updating_camp_list[4], updating_camp_list[5], updating_camp_list[6]))
+
+    update_emergency_table.pack()
+
+
 
 
 
