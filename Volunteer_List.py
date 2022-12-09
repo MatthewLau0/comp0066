1e049c4a99680e20235f1626f8c64e5dc5ebca8d
MusLG <luke.muschialli.20@ucl.ac.uk>
2022-12-09 17:23:17 +0000

    New

--- Volunteer_List.py
+++ Volunteer_List.py
@@ -127,6 +127,10 @@ def volunteerList(screen):
         global full_name
         global DOB
         global age
+        global full_name_label
+        global DOB_calendar
+        global today
+
 
         select_camp = StringVar()
         full_name = StringVar()
@@ -180,6 +184,19 @@ def volunteerList(screen):
         DOB = datetime.datetime.strptime(DOB_calendar.get_date(), "%d/%m/%Y").date()
 
 
+
+        emergency_submit_button = Button(volunteer_entry_screen, text="Submit", command=confirmAge)
+        emergency_submit_button.pack()
+
+    def confirmAge():
+        global volunteer_age
+        global DOB
+        global DOB_calendar
+        global today
+
+
+        DOB = datetime.datetime.strptime(DOB_calendar.get_date(), "%d/%m/%Y").date()
+
         volunteer_age = 0
 
         if DOB.month < today.month and today.year > DOB.year:
@@ -196,13 +213,8 @@ def volunteerList(screen):
 
         volunteer_age = str(volunteer_age)
 
-        availability_label = Label(volunteer_entry_screen, text="Please enter your weekly availability")
-        availability_label.pack()
-        availability_entry = Entry(volunteer_entry_screen, textvariable=volunteer_availability)
-        availability_entry.pack()
+        newvolunteerVerify()
 
-        emergency_submit_button = Button(volunteer_entry_screen, text="Submit", command=newvolunteerVerify)
-        emergency_submit_button.pack()
 
 
     def newvolunteerVerify():
@@ -224,8 +236,12 @@ def volunteerList(screen):
         global gender
         global age
         global volunteer_availability
+        global full_name
+        global full_name_label
 
-        if '@' not in email.get() or '.' not in email.get():
+        if len(full_name.get()) == 0 or full_name.get() == ' ':
+            full_name_label.config(text="Please enter a gender. If you prefer not to specify a gender, enter n/a.", fg='#f00')
+        elif '@' not in email.get() or '.' not in email.get():
             email_label.config(text="Please enter a valid email address", fg='#f00')
         elif len(phone_area_code.get())>4:
             phone_number_label.config(text="Please enter a valid phone area code (including a + symbol) and a valid phone number", fg='#f00')
@@ -233,8 +249,6 @@ def volunteerList(screen):
             phone_number_label.config(text="Please enter a valid phone area code (including a + symbol) and a valid phone number", fg='#f00')
         elif len(gender.get()) == 0 or gender.get() == ' ':
             gender_label.config(text="Please enter a gender. If you prefer not to specify a gender, enter n/a.", fg='#f00')
-        elif len(volunteer_availability.get()) == 0 or (volunteer_availability.get() == ' '):
-            availability_label.config(text="Please enter a valid availabilty", fg='#f00')
         else:
             createvolunteerSubmit()
 
