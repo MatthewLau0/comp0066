bad85203bc5bae497e1ff4e3913760e9594017ba
alexdunn <alexander.dunn.20@ucl.ac.uk>
2022-12-12 15:22:54 +0000

    added accounts amendment page

--- Accounts - Alex.py
+++ Accounts - Alex.py
@@ -155,7 +155,99 @@ for i in volunteer_list:
     active_list.append = []
 
 def delete_volunteer(volunteer)
-    del 
+    del
+
+
+
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#allowing volunteers to edit their own information including name, phone number, DOB,
+
+ def update_volunteer_age():
+        update_block_screen_age = Toplevel()
+        update_block_screen_age.title("Update Age Select")
+
+        id_list = []
+
+        for i in blocks_list:
+            id_list.append(i[1])
+
+        block_id = StringVar()
+        block_name = StringVar()
+        block_cap = StringVar()
+        block_occ = StringVar()
+        block_location = StringVar()
+
+        id_select_label = Label(update_block_screen_id, text="Please choose a block ID to update")
+        id_select_label.pack()
+        block_id.set("Select ID")
+        id_select_option = OptionMenu(update_block_screen_id, block_id, *id_list)
+        id_select_option.pack()
+
+#instead of showing table for volunteer to edit, show the table from Luke's registering in Volunteer_List, which they can edit directly
+#the information - need to do that next
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def activate_volunteer("volunteers.txt"):
+    with volunteer_file = open("volunteers.txt", "r") as f:
+        volunteer_list = f.read()
+
+    for line in volunteer_file:
+        line_list = line_split("%")
+        volunteer_list.append(line_list)
+    volunteer_file.close()
+
+    if volunteer_list[-2] == 'Deactivated':
+        deactivated_volunteer_list = [(volunteer_list[1], volunteer_list[-2])]
+
+    print(deactivated_volunteer_list)
+
+volunteer_ID = input('Enter Volunteer ID: ')
+
+    for i in volunteer_list:
+        if volunteer_ID in volunteer_list:
+            index = volunteers_list.index(volunteer_ID)
+            volunteer_list[index] = volunteer_list[index].replace('Deactivated, Active')
+
+    print(volunteer_list)
+
+    volunteer_file = open("volunteers.txt", "r")
+    volunteer_list = []
+    for line in volunteer_file:
+        line_list = line.split('%')
+        volunteer_list.append(line_list)
+    volunteer_file.close()
+
+
+
+def deactivate_volunteer("volunteers.txt"):
+    with volunteer_file = open("volunteers.txt", "r") as f:
+        volunteers_list = f.read()
+
+    for line in volunteers_file:
+        line_list = line_split("%")
+        volunteer_list.append(line_list)
+    volunteer_file.close()
+
+    if volunteers_list[-2] == 'Active':
+        deactivated_volunteers_list = [(volunteers_list[1], volunteers_list[-2])]
+
+    print(active_volunteers_list)
+
+
+volunteer_ID = input('Enter Volunteer ID: ')
+
+for i in volunteers_list:
+    if volunteer_ID in volunteers_list:
+        index = volunteers_list.index(volunteer_ID)
+        volunteers_list[index] = volunteers_list[index].replace('Active', 'Deactivated')
+
+print(volunteers_list)
+
+
+
+
 
 
 
