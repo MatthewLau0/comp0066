1d204ceb5e4add84f2e815565483c2ad63aab442
MusLG <luke.muschialli.20@ucl.ac.uk>
2022-12-14 19:08:13 +0000

    New

--- Final_Files/Admin_Home.py
+++ Final_Files/Admin_Home.py
@@ -27,34 +27,29 @@ def adminHome():
 
         app_info_screen = Toplevel(admin_home)
         app_info_screen.title("User Guide")
+        app_info_screen.geometry("1200x750")
 
-        screen_width3 = app_info_screen.winfo_screenwidth()
-        screen_height3 = app_info_screen.winfo_screenheight()
-        window_height3 = screen_height3
-        window_width3 = 600
-
-        center_x3 = int(screen_width3 / 2 - window_width3 / 2)
-        center_y3 = int(screen_height3 / 2 - window_height3 / 2)
-        app_info_screen.geometry(f'{window_width3}x{window_height3}+{center_x3}+{center_y3}')
 
         Label(app_info_screen, text="\nHow to use the Admin Homepage", font=("Avenir", 44)).pack()
 
         Label(app_info_screen, text="\nCreate New Emergency Camp", font=("Avenir", 24, "bold")).pack()
 
-        Label(app_info_screen, text="\n This page can be used when a new emergency has occured, and you need to create a new camp for volunteers to assign refugees affected by the emergency. \nHere you can create a camp name, register the type of emergency, description of the emergency, area affected, start date and activation status.").pack()
+        Label(app_info_screen, text="\nThis page can be used when a new emergency has occured, and you need to create a new camp for volunteers to assign refugees affected by the emergency. \nHere you can create a camp name, register the type of emergency, description of the emergency, area affected, start date and activation status.").pack()
 
-        Label(app_info_screen, text="\n\nUpdate Existing Emergency Camp", font=("Avenir", 24, "bold")).pack()
+        Label(app_info_screen, text="\nUpdate Existing Emergency Camp", font=("Avenir", 24, "bold")).pack()
 
-        Label(app_info_screen, text="\n This page can be used to update the details of an existing emergency camp. \nAll details (except for camp name and camp ID, which are unique) can be updated, and you can update the status to close the emergency if the situation has resolved.").pack()
+        Label(app_info_screen, text="\nThis page can be used to update the details of an existing emergency camp. \nAll details (except for camp name and camp ID, which are unique to each camp) can be updated. \n Importantly, you can update the status to close the emergency if the situation has resolved.").pack()
 
-        Label(app_info_screen, text="\nVolunteer", font=("Avenir", 24, "bold")).pack()
+        Label(app_info_screen, text="\nManage Volunteers", font=("Avenir", 24, "bold")).pack()
 
-        Label(app_info_screen, text="Sample Text\nSample Text\nSample Text\nSample Text\n\n\n\n").pack()
+        Label(app_info_screen, text="\nThis page can be used to view all the volunteers currently in the LAMSA database across all camps.\n You can also use this page to activate, deactivate and delete volunteer accounts.").pack()
+
+        Label(app_info_screen, text="\nSummary of Existing Camps", font=("Avenir", 24, "bold")).pack()
+
+        Label(app_info_screen, text="\nThis page can be used to view all of the current camps that are in the LAMSA database, to view the layout of an individual camp \n(including medical supplies, rations, toilets and accommodation) and to view the refugees currently in the camp.\n\n").pack()
 
         back_button = Button(app_info_screen, text="Close", command=app_info_screen.destroy, width=30, height=2)
         back_button.pack()
-
-        app_info_screen.mainloop()
 
     admin_home = Tk()
     admin_home.geometry("650x650")
@@ -83,3 +78,5 @@ def adminHome():
 
 
     admin_home.mainloop()
+
+adminHome()
