f6e21612bed71465f3fe464e5a632810e004d301
MatthewLau0 <109472718+MatthewLau0@users.noreply.github.com>
2022-12-18 10:11:15 -0300

    Update Volunteer_Add_Availability.py

--- Final_Files_Reduced/Volunteer_Add_Availability.py
+++ Final_Files_Reduced/Volunteer_Add_Availability.py
@@ -24,7 +24,7 @@ def add_calendar():
 
 
     #LIST WITH THE TIMINGS
-    times = ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45', '03:00', '03:15', '03:30', '03:45', '04:00', '04:15', '04:30', '04:45', '05:00', '05:15', '05:30', '05:45', '06:00', '06:15', '06:30' , '06:45' , '07:00', '07:15' , '07:30' , '07:45' , '08:00', '08:15' , '08:30' , '08:45' , '09:00', '09:15' , '09:30' , '09:45' , '10:00', '10:15' , '10:30' , '10:45' , '11:00', '11:15' , '11:30' , '11:45' , '12:00', '12:15' , '12:30' , '12:45' , '13:00', '13:15' , '13:30' , '13:45' , '14:00', '14:15' , '14:30' , '14:45' , '15:00', '15:15' , '15:30' , '15:45' , '16:00', '16:15' , '16:30' , '16:45' , '17:00', '17:15' , '17:30' , '17:45' , '18:00', '18:15' , '18:30' , '18:45' , '19:00', '19:15' , '19:30' , '19:45' , '20:00', '20:15' , '20:30' , '20:45' , '21:00', '21:15' , '21:30' , '21:45' , '22:00', '22:15' , '22:30' , '22:45' , '23:00', '23:15' , '23:30' , '23:45' ]
+    times = ['', '00:00', '00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00', '02:15', '02:30', '02:45', '03:00', '03:15', '03:30', '03:45', '04:00', '04:15', '04:30', '04:45', '05:00', '05:15', '05:30', '05:45', '06:00', '06:15', '06:30' , '06:45' , '07:00', '07:15' , '07:30' , '07:45' , '08:00', '08:15' , '08:30' , '08:45' , '09:00', '09:15' , '09:30' , '09:45' , '10:00', '10:15' , '10:30' , '10:45' , '11:00', '11:15' , '11:30' , '11:45' , '12:00', '12:15' , '12:30' , '12:45' , '13:00', '13:15' , '13:30' , '13:45' , '14:00', '14:15' , '14:30' , '14:45' , '15:00', '15:15' , '15:30' , '15:45' , '16:00', '16:15' , '16:30' , '16:45' , '17:00', '17:15' , '17:30' , '17:45' , '18:00', '18:15' , '18:30' , '18:45' , '19:00', '19:15' , '19:30' , '19:45' , '20:00', '20:15' , '20:30' , '20:45' , '21:00', '21:15' , '21:30' , '21:45' , '22:00', '22:15' , '22:30' , '22:45' , '23:00', '23:15' , '23:30' , '23:45' ]
 
     def delete1():
         availability_screen.destroy()
@@ -282,26 +282,22 @@ def add_calendar():
         sunday_text = IntVar()
 
         #EACH INDIVIDUAL FUNCTION IS EACH DAY OF THE WEEK
-        def mon():
-            global monday_initial_time
-            global monday_final_time
-            monday_initial_time = StringVar()
-            monday_final_time = StringVar()
-            if monday_text.get() == 1:
-                monday_initial_time_menu = OptionMenu(choose_days_screen, monday_initial_time, *times)
-                monday_initial_time_menu.place(x = 180, y = 280, width = 100)
+        
+            
+        monday_initial_time = StringVar()
+        monday_final_time = StringVar()
+            
+        monday_initial_time_menu = tkinter.ttk.Combobox(choose_days_screen, variable = monday_initial_time, values = *times)
+        monday_initial_time_menu.place(x = 180, y = 280, width = 100)
 
-                monday_label = Label(choose_days_screen, text= "to")
-                monday_label.place(x = 305, y = 280)
+        monday_label = Label(choose_days_screen, text= "to")
+        monday_label.place(x = 305, y = 280)
 
 
-                monday_final_time_menu = OptionMenu(choose_days_screen, monday_final_time, *times)
-                monday_final_time_menu.place(x=350, y=280, width=100)
+        monday_final_time_menu = OptionMenu(choose_days_screen, monday_final_time, *times)
+        monday_final_time_menu.place(x=350, y=280, width=100)
 
-            if monday_text.get() == 0:
-                lol_label = Label(choose_days_screen, text='')
-                lol_label.place(x=160, y=280, width=800, height=40)
-
+            
         def tues():
             global tuesday_initial_time
             global tuesday_final_time
